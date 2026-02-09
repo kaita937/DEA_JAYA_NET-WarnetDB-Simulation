93456e7e7095f12e842ea50d8d98a3d744ba554c
Zaki <112177143+kaita937@users.noreply.github.com>
2026-02-10 06:20:04 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,6 @@
-import java.io.FileWriter;
-import java.io.PrintWriter;
-
 public class Main {
     public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileWriter("log.txt", true))) {
-            out.println("=== APLIKASI DIJALANKAN ===");
-            out.println("java.version = " + System.getProperty("java.version"));
-            out.println("java.home    = " + System.getProperty("java.home"));
-            out.println("user.dir     = " + System.getProperty("user.dir"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+    
         javax.swing.SwingUtilities.invokeLater(() -> {
             new MainFrame().setVisible(true);
         });
